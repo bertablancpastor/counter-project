793fc53107728b4553cb8674b5462d2575b41a82
bertablancpastor <bertablanc89@gmail.com>
2023-10-23 13:57:42 +0200

    finish

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 // Tu código aquí.
+// const lowCount = document.querySelector(".prevBtn");
+// const addCount = document.querySelector(".nextBtn");
+const counter = document.querySelector("#counter");
+let count = 0;
+
+function lowerCount() {
+  count--;
+  counter.innerHTML = count;
+
+  count < 0 ? (counter.style.color = "red") : (counter.style.color = "green");
+}
+
+function addCount() {
+  console.log("Click on ADD COUNTER");
+  count++;
+  counter.innerHTML = count;
+
+  count > 0 ? (counter.style.color = "green") : (counter.style.color = "red");
+}
